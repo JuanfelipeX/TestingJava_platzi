44d18f8e770d0050cd0f3ec6529f801fdc3ad5e6
PipesSex <pipesjimenez6@gmail.com>
2022-01-05 08:14:00 -0500

    arreglando y mejorando el nombre de las funciones y haciendio una funcion en negativo

--- src/test/java/com/platzi/javatest/util/StringUtilTest.java
+++ src/test/java/com/platzi/javatest/util/StringUtilTest.java
@@ -8,8 +8,24 @@ import org.junit.Test;
 public class StringUtilTest {
 
     @Test
-        public void testRepeat() {
-            Assert.assertEquals("holaholahola", StringUtil.repeat("hola", 3));
+        public void repeat_string_once() {
             Assert.assertEquals("hola", StringUtil.repeat("hola", 1));
     }
+
+    @Test
+    public void repeat_string_multiple_times() {
+        Assert.assertEquals("holahola", StringUtil.repeat("hola", 3));
+    }
+
+    @Test
+    public void repeat_string_zero_times() {
+        Assert.assertEquals("", StringUtil.repeat("hola", 0));
+    }
+
+    @Test (expected = IllegalAccessException.class)
+    public void repeat_string_negative_times() {
+        Assert.assertEquals("", StringUtil.repeat("hola", -3));
+    }
+
+
 }
